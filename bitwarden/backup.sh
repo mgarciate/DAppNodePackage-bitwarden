e80c641866ac76176d0f7c9c95c672a37ebf8d86
mgarciate <mgarciate@gmail.com>
2023-02-05 08:49:25 +0100

    Upload files to web3.storage

--- bitwarden/backup.sh
+++ bitwarden/backup.sh
@@ -2,6 +2,8 @@
 
 # Get all data from the previous backup
 
+# Create a variable from an environment variable or use a default value
+
 # Login
 /bw config server http://bitwarden.public.dappnode
 /bw login $ACCOUNT_EMAIL $ACCOUNT_PASSWORD
@@ -13,11 +15,18 @@ sessionId=$(/bw unlock whatever1 --raw)
 /bw export --output /tmp/myvault.json --format encrypted_json --session $sessionId
 openssl aes-128-cbc -in myvault.json -out myvault.json.crypted -pass pass:$FILE_PASSWORD
 split -a 3 -n 32 myvault.json.crypted chunk.
+# Declare array of strings
+file_crypted_cids=()
 for f in /tmp/chunk.???; do 
-    openssl aes-128-cbc -in $f -out $f.crypted -pass pass:$FILE_PASSWORD;
-    # Upload $f.crypted to web3.storage
-    # Store the hash from web3.storage
+    file_crypted = $f.crypted
+    openssl aes-128-cbc -in $f -out file_crypted -pass pass:$FILE_PASSWORD;
+    file_crypted_cid=$(curl -X POST -H "Authorization: Bearer $WEB3STORAGE_API_TOKEN" -F "file=@$file_crypted" https://api.web3.storage/upload | jq '.cid' --raw-output)
+    echo "new $file_crypted: $file_crypted_cid"
+    file_crypted_cids+=( "$file_crypted_cid" )
 done
+# Create manifest.json with all hashes that are in file_crypted_cids strings array
+
+
 # Create manifest.json with all hashes (.json)
 # Encrypt manifest.json to manifest.json.crypted
 # Upload manifest.json.crypted to web3.storage
@@ -33,3 +42,7 @@ done
 
 # Logout
 /bw logout
+
+strings=( "string1" "string2" "string3" )
+array_of_strings=$(jq -n --argjson strings "${strings[@]}" '{"cid": $strings | map({"value": .})}')
+echo "$array_of_strings" > array_of_strings.json
