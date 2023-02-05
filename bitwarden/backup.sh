f8565c3e13b0cd7b3cfdddc8fb135660b8fd4a6b
mgarciate <mgarciate@gmail.com>
2023-02-05 19:32:04 +0100

    Update backup script

--- bitwarden/backup.sh
+++ bitwarden/backup.sh
@@ -4,33 +4,37 @@
 
 # Create a variable from an environment variable or use a default value
 
+cd /tmp
+
 # Login
 /bw config server http://bitwarden.public.dappnode
 /bw login $ACCOUNT_EMAIL $ACCOUNT_PASSWORD
 sessionId=$(/bw unlock whatever1 --raw)
-# get the sessionId from here
 
 /bw sync
 # /bw export --output /tmp/myvault.json --format encrypted_json --password whatever1
 /bw export --output /tmp/myvault.json --format encrypted_json --session $sessionId
 openssl aes-128-cbc -in myvault.json -out myvault.json.crypted -pass pass:$FILE_PASSWORD
-split -a 3 -n 32 myvault.json.crypted chunk.
+split -a $SUFFIX_SIZE -n $N_PARTS myvault.json.crypted chunk.
 # Declare array of strings
 file_crypted_cids=()
 for f in /tmp/chunk.???; do 
-    file_crypted = $f.crypted
-    openssl aes-128-cbc -in $f -out file_crypted -pass pass:$FILE_PASSWORD;
+    file_crypted=$f.crypted
+    echo "openssl aes-128-cbc -in $f -out $file_crypted -pass pass:$FILE_PASSWORD"
+    openssl aes-128-cbc -in $f -out $file_crypted -pass pass:$FILE_PASSWORD
+    # Upload file with curl command
     file_crypted_cid=$(curl -X POST -H "Authorization: Bearer $WEB3STORAGE_API_TOKEN" -F "file=@$file_crypted" https://api.web3.storage/upload | jq '.cid' --raw-output)
     echo "new $file_crypted: $file_crypted_cid"
     file_crypted_cids+=( "$file_crypted_cid" )
 done
-# Create manifest.json with all hashes that are in file_crypted_cids strings array
 
-
-# Create manifest.json with all hashes (.json)
-# Encrypt manifest.json to manifest.json.crypted
-# Upload manifest.json.crypted to web3.storage
-# Store the hash from web3.storage
+# Improve the json file with a json array in cids object and a new object called version with value "1"
+json_array=$(printf '%s\n' "${file_crypted_cids[@]}" | jq -R . | jq -s .)
+echo $json_array > manifest.json
+jq -n --argjson cids "$(cat manifest.json)" '{"cids": $cids,"version": "1"}' > manifest.json
+openssl aes-128-cbc -in manifest.json -out manifest.json.crypted -pass pass:$FILE_PASSWORD
+file_crypted=$(realpath manifest.json.crypted)
+file_crypted_cid=$(curl -X POST -H "Authorization: Bearer $WEB3STORAGE_API_TOKEN" -F "file=@$file_crypted" https://api.web3.storage/upload | jq '.cid' --raw-output)
 # Encrypt the hash with my ETH public key
 # Update the contract
 
